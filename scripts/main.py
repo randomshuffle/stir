bb9c056097758e02ec1270efc3fe2348aaa69f3b
Anubhav Baweja <abaweja@seas.upenn.edu>
2025-05-07 10:13:06 -0400

    script mods

--- scripts/main.py
+++ scripts/main.py
@@ -4,32 +4,31 @@ from stir import stir_round, run_STIR
 from aurora import run_aurora_FRI, run_aurora_STIR
 
 def plot_table_values():
-    secparam = 128
+    secparams = [128]
     hashsize = 256
-    pow = 22
-    log_degrees = [18, 20, 22, 24, 26, 28, 30]
-    fieldsizebits = 192
+    pow = 0
+    log_degrees = [20, 22, 24, 26, 28, 30]
+    field_size_bits = 192
     stopping_condition = 2**6
     conj = 1
-    rate_bits = [1, 2, 3, 4]
+    rate_bits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     result = []
-    for r_bits in rate_bits:
-        new_row = []
-        for log_degree in log_degrees:
-            ldt_params = LDTParameters(secparam=secparam, hashsize=hashsize, pow=pow, log_degree=log_degree, field_size_bits=fieldsizebits, rho_bits = r_bits, conj=conj)
-            sum_fri = run_aurora_FRI(ldt_params, fold=[2,8], stopping_condition=stopping_condition)
-            #fri.print()
-            sum_fri_size = round(sum_fri.argument_size() / 8192)
-            sum_stir = run_aurora_STIR(ldt_params, fold=[2,16], max_len_ratio=[2], stopping_condition=stopping_condition)
-            #sum_stir.print()
-            sum_stir_size = round(sum_stir.argument_size() / 8192)
-            ratio = round(sum_fri_size/sum_stir_size, 2)
-            new_row.append('$\\frac{{ {} }}{{ {} }} \\approx {} \\times $'.format(sum_fri_size, sum_stir_size, ratio))
-        result.append(new_row)
-
-    return '\\\\\n'.join([' & '.join(["$\\sfrac{{1}}{{{}}}$".format(2**rate)] + row) for rate,row in zip(rate_bits, result)])
-
+    for secparam in secparams:
+        for r_bits in rate_bits:
+            new_row = []
+            for log_degree in log_degrees:
+                ldt_params = LDTParameters(secparam=secparam, hashsize=hashsize, pow=pow, log_degree=log_degree, field_size_bits=field_size_bits, rho_bits = r_bits, conj=conj)
+                # sum_fri = run_aurora_FRI(ldt_params, fold=[2,8], stopping_condition=stopping_condition)
+                # fri.print()
+                # sum_fri_size = round(sum_fri.argument_size() / 8192)
+                sum_stir = run_STIR(ldt_params, fold=[16], max_len_ratio=[1,2], stopping_condition=stopping_condition)
+                # sum_stir.print()
+                sum_stir_size = sum_stir.argument_size()
+                # ratio = round(sum_fri_size/sum_stir_size, 2)
+                new_row.append('{}'.format(sum_stir_size))
+            result.append(new_row)
+            print("secparam: {}, rbits: {}, size: {}".format(secparam, r_bits, new_row))
 
 if __name__ == '__main__':
     print(plot_table_values())
---
+++ scripts/tensor.py
@@ -0,0 +1,155 @@
+from ldt_params import LDTParameters
+from stir import run_STIR
+from math import log2, ceil, floor, log
+
+common_params = {
+    'secparam': 128,
+    'hashsize': 256,
+    'field_size_bits': 192,
+}
+
+# Initial code parameters
+horizontal_code_params = {
+    'rate_bits': 3,
+    'dist': 1 - 2**(-3),
+}
+vertical_code_params = {
+    'rate_bits': 1,
+    'dist': 0.5,
+}
+
+tensor_params = {
+    'log_k': 30,
+    'rate_bits': horizontal_code_params['rate_bits'] + vertical_code_params['rate_bits'],
+    'dist': horizontal_code_params['dist'] * vertical_code_params['dist'],
+    'num_col_checks': ceil(common_params['secparam'] / horizontal_code_params['rate_bits']), 
+    'num_row_checks': None,
+    'log_horizontal_k': None,
+    'log_vertical_k': None,
+}
+tensor_params['num_row_checks'] = 1 # ceil(1 / horizontal_code_params['dist'])
+tensor_params['log_horizontal_k'] = ceil((tensor_params['log_k'] + log2(tensor_params['num_col_checks'])) / 2)
+tensor_params['log_vertical_k'] = tensor_params['log_k'] - tensor_params['log_horizontal_k']
+
+# Final code parameters, should just be Reed Solomon
+final_code_params = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'rate_bits': None,
+    'dist': None,
+}
+final_code_params['rate_bits'] = tensor_params['log_k'] + tensor_params['rate_bits'] - final_code_params['log_k']
+final_code_params['dist'] = 1 - 2**(-final_code_params['rate_bits'])
+
+
+sumcheck1_params = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'degree': 3,
+}
+sumcheck2_params = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'degree': 2,
+}
+
+codeswitch_params = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'rate_bits': horizontal_code_params['rate_bits'],
+}
+
+batching_params = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'num_batch': 3,
+    'rate_bits': final_code_params['rate_bits'],
+    'batch_loss_bits': final_code_params['rate_bits'] - 2, # this is assuming we have good batching for rs codes in list decoding
+}
+
+stir_params_common = {
+    'pow': 0,
+    'stopping_condition': 2**10,
+    'conj': 1,
+    'fold': 4,
+    'max_len_ratio': 2,
+}
+
+stir_params1 = {
+    'log_k': tensor_params['log_horizontal_k'],
+    'rate_bits': final_code_params['rate_bits'],
+}
+
+stir_params2 = {
+    'log_k': tensor_params['log_k'],
+    'rate_bits': tensor_params['rate_bits'],
+}
+
+def proof_size(message_size, rate_bits, hash_size, queries):
+    print(f"Message size: {message_size}, Rate bits: {rate_bits}, Hash size: {hash_size}, Queries: {queries}")
+    return (message_size + rate_bits) * queries * hash_size
+
+if __name__ == '__main__':
+
+    queries_to_w_for_lit_reduction = tensor_params['num_col_checks'] * tensor_params['num_row_checks']
+    queries_to_wprime_for_lit_reduction = tensor_params['num_col_checks']
+    x1 = proof_size(tensor_params['log_k'], tensor_params['rate_bits'], common_params['hashsize'], queries_to_w_for_lit_reduction)
+    x2 = proof_size(tensor_params['log_horizontal_k'], horizontal_code_params['rate_bits'], common_params['hashsize'], queries_to_wprime_for_lit_reduction)
+    print("LIT reduction argument size:")
+    print(x1)
+    print(x2)
+
+    sumcheck_field_elements = sumcheck1_params['degree'] * sumcheck1_params['log_k'] + sumcheck2_params['degree'] * sumcheck2_params['log_k']
+    x3 = sumcheck_field_elements * common_params['field_size_bits']
+    print("Sumcheck argument size:")
+    print(x3)
+
+    # queries_codeswitch = ceil(common_params['secparam'] / codeswitch_params['rate_bits'])
+    # x4 = proof_size(codeswitch_params['log_k'], codeswitch_params['rate_bits'], common_params['hashsize'], queries_codeswitch)
+    # print("Codeswitch argument size:")
+    # print(x4)
+    
+    queries_batching = ceil(batching_params['num_batch'] * common_params['secparam'] / batching_params['batch_loss_bits'])
+    x5 = proof_size(batching_params['log_k'], batching_params['rate_bits'], common_params['hashsize'], queries_batching)
+    print("Batching argument size:")
+    print(x5)
+
+    ldt_params1 = LDTParameters(
+        secparam=common_params['secparam'],
+        hashsize=common_params['hashsize'],
+        pow=stir_params_common['pow'],
+        log_degree=stir_params1['log_k'],
+        field_size_bits=common_params['field_size_bits'],
+        rho_bits=stir_params1['rate_bits'],
+        conj=stir_params_common['conj']
+    )
+    sum_stir1 = run_STIR(
+        ldt_params1,
+        fold=[stir_params_common['fold']],
+        max_len_ratio=[stir_params_common['max_len_ratio']],
+        stopping_condition=stir_params_common['stopping_condition']
+    )
+    x6 = sum_stir1.argument_size()
+    print("STIR argument size:")
+    print(x6)
+
+    print("Total new argument size:")
+    print(x1 + x2 + x3 + 0 + x5 + x6)
+
+    print("\nVS\n")
+
+    ldt_params2 = LDTParameters(
+        secparam=common_params['secparam'],
+        hashsize=common_params['hashsize'],
+        pow=stir_params_common['pow'],
+        log_degree=stir_params2['log_k'],
+        field_size_bits=common_params['field_size_bits'],
+        rho_bits=stir_params2['rate_bits'],
+        conj=stir_params_common['conj']
+    )
+    sum_stir2 = run_STIR(
+        ldt_params2,
+        fold=[stir_params_common['fold']],
+        max_len_ratio=[stir_params_common['max_len_ratio']],
+        stopping_condition=stir_params_common['stopping_condition']
+    )
+    print("Original STIR argument size:")
+    print(sum_stir2.argument_size())
+
+    print("Ratio of new to old argument size:")
+    print((x1 + x2 + x3 + 0 + x5 + x6) / sum_stir2.argument_size())
